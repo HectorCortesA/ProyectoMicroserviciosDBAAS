455cf0b789bc4b1e30796f8ee4f89a9e0fc1adc1
Hector Cortes <hectordroid903@gmail.com>
2026-05-10 22:47:55 -0600

    auth

---
+++ app/database/connection.py
@@ -0,0 +1,12 @@
+from pymongo import MongoClient
+
+from app.config import (
+    MONGO_URL,
+    DATABASE_NAME
+)
+
+client = MongoClient(MONGO_URL)
+
+db = client[DATABASE_NAME]
+
+users_collection = db["users"]
